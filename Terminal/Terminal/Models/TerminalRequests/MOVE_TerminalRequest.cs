cbab7ef160cf5933d40a6a76d441cbcd757be803
IllyaVakulenko <vacodenko@gmail.com>
2023-06-16 12:29:35 +0300

    Add MOVE template and add MOVE commandHandler to Init() in Program.cs

---
+++ Terminal/Terminal/Models/TerminalRequests/MOVE_TerminalRequest.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Terminal.Handlers;
+using Terminal.Helpers;
+using Terminal.Models.TerminalRequests.Base;
+
+namespace Terminal.Models.TerminalRequests
+{
+    internal class MOVE_TerminalRequest : TerminalRequest
+    {
+        public MOVE_TerminalRequest()
+        {
+            CommandName = "move";
+            Description = "{url + name || name} {url + name}";
+        }
+        public override void Execute(CommandHandler handler, string commandBody = "")
+        {
+            try
+            {
+                
+            }
+            catch (Exception ex)
+            {
+                ConsoleHelper.WriteColorLine($"Error: {ex.Message}", ConsoleColor.DarkRed);
+            }
+        }
+    }
+}
